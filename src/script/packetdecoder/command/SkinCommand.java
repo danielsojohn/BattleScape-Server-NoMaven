5cfeadc96a5a4c5a97402776ee1fc3e07e946a25
Miika <miika.hagelberg@gmail.com>
2019-09-25 06:28:05 +0300

    Add command to manually set skin color

---
+++ src/script/packetdecoder/command/SkinCommand.java
@@ -0,0 +1,26 @@
+
+
+package script.packetdecoder.command;
+
+import com.palidino.osrs.Main;
+import com.palidino.osrs.io.Command;
+import com.palidino.osrs.model.player.Player;
+import lombok.var;
+
+public class SkinCommand implements Command {
+    @Override
+    public String getExample() {
+        return "id";
+    }
+
+    @Override
+    public boolean canUse(Player player) {
+        return player.getRights() == Player.RIGHTS_ADMIN || Main.eventPriviledges(player);
+    }
+
+    @Override
+    public void execute(Player player, String message) {
+        var id = Integer.parseInt(message);
+        player.getAppearance().setColor(4, id);
+    }
+}
