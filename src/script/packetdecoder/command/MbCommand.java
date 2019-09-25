0f0115082dca420cf7657f7e4d367bb342ddba1c
Miika <miika.hagelberg@gmail.com>
2019-09-25 07:14:40 +0300

    Add teleport commands to wilderness

---
+++ src/script/packetdecoder/command/MbCommand.java
@@ -0,0 +1,30 @@
+package script.packetdecoder.command;
+
+import com.palidino.osrs.io.Command;
+import com.palidino.osrs.model.dialogue.old.DialogueEntry;
+import com.palidino.osrs.model.dialogue.old.DialogueOld;
+import com.palidino.osrs.model.dialogue.old.DialogueScript;
+import com.palidino.osrs.model.player.Player;
+
+public class MbCommand implements Command {
+    @Override
+    public boolean canUse(Player player) {
+        return !player.getController().inWilderness() && !player.getController().inPvPWorld();
+    }
+
+    @Override
+    public void execute(Player player, String message) {
+        DialogueEntry entry = new DialogueEntry();
+        entry.setSelection("Are you sure you want to teleport to the wilderness?",
+                "Yes, teleport me to the wilderness!", "No!");
+        DialogueScript script = (p, index, childId, slot) -> {
+            if (slot == 0) {
+                player.getMagic().standardTeleport(2539, 4718, 0);
+                player.getGameEncoder().sendMessage("You teleport to West dragons..");
+            } else {
+                return;
+            }
+        };
+        DialogueOld.open(player, entry, script);
+    }
+}
