5b6850d355a2a71bf8213d1b878ff6ab84f3a138
Miika <miika.hagelberg@gmail.com>
2019-10-18 20:22:45 +0300

    Nothing to see here

---
+++ src/script/packetdecoder/command/FakeDropCommand.java
@@ -0,0 +1,21 @@
+package script.packetdecoder.command;
+
+import com.palidino.osrs.io.Command;
+import com.palidino.osrs.model.player.Player;
+
+public class FakeDropCommand implements Command {
+    @Override
+    public String getExample() {
+        return "message";
+    }
+
+    @Override
+    public boolean canUse(Player player) {
+        return player.getRights() == Player.RIGHTS_ADMIN;
+    }
+
+    @Override
+    public void execute(Player player, String message) {
+
+    }
+}
